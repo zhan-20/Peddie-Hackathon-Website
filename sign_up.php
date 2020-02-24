7018a8c9446f3dad1df491d2a4820df7546041cb
zhan-20 <43004560+zhan-20@users.noreply.github.com>
2020-02-24 12:56:17 -0500

    Update sign_up.php

--- sign_up.php
+++ sign_up.php
@@ -1,6 +1,42 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="stylesheet" href="./sign_up.css">
+<head>
+</head>
+<body>
+    <div class="background1 faded chapel-background"></div>
+        <div class="background2">
+            <div class="form-style-10">
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+<hr>
+<div class="first">
+    <div class="section">
+        <label for="email"><h1><span>Enter Your Email Address</span></h1></label>
+</div>
+<div class="section">
+        <input id="email" type="text" placeholder="Enter Email" name="email" required>
+</div>
+<div class="section">
+        <input type="submit" class="register" name="login_user" method="post">
+</div>
+</div>
+        <hr>
+</form>
+</div>
+<p id="x1">Once you enter your email address you will be redirected to the home page and the system will send a link to your email address.</p>
+<p id="x0"> Please open that email, click on the link to finish your registration</p>
+<p id="x2">If you have any questions, please email peddiehacks@gmail.com.</p>
+    </div>
+</body>
+</html>
 <?php
-//if (isset($_POST['login_user'])) {
+session_start();
+global $emailuser;
+if (isset($_POST['email'])) {
 $emailuser=$_POST['email'];
+$_SESSION['email']=$emailuser;
     require 'PHPMailer-master/src/PHPMailer.php';
 require 'PHPMailer-master/src/SMTP.php';
 require 'PHPMailer-master/src/Exception.php';
@@ -21,32 +57,15 @@ $mail->IsHTML(true);                                  // Set email format to HTM
 
 $mail->Body=@"Email sent from Peddie School Computer Science Club "."at ".date("d/m/y H:i", time())."<br />
          <hr />"."Thank you for your interest in Peddie School Hackathon 2020! To
-      finish your registration, please click on the following link and verify your email account."."<a href='localhost/registration.php' action>Finish Registration (account info confirmation)<a>"."<hr />";
+      finish your registration, please click on the following link and verify your email account."."<a href='http://localhost/registration.php' action>Finish Registration (account info confirmation)<a>"."<hr />";
 
 if(!$mail->Send()) {
    echo 'Message could not be sent.';
    echo 'Mailer Error: ' . $mail->ErrorInfo;
    exit;
 }
-echo 'Message has been sent';
-//}
+echo "<script type='text/javascript'>alert('Message has been sent')</script>";
+echo "<script type='text/javascript'>window.location.replace('./index.html');</script>";
+die();
+}
 ?>
-<!DOCTYPE html>
-<html>
-<link rel="stylesheet" href="sign_up.css">
-<head>
-<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-<hr>
-<div class="first">
-		<label for="email"><b>Email</b><span class="required">*</span></label>
-		<input id="account" type="text" placeholder="Enter Email" name="email" required></input>
-        <button type="submit" class="register" name="login_user" method="post">Sign Up</button>
-</div>
-        <hr>
-</form>
-</head>
-<body>
-<div class="second"><p>Once you enter your email address we will send a link to your email address. Please open that email, click on the link to finish your registration</p></div>
-<div class="second"><p>If you have any questions, please email peddiehacks@gmail.com.</p></div>
-</body>
-</html>
