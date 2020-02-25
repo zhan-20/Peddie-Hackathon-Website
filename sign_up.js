1365d33880efa91e451830cc4d033f6e727b8cb9
zhan-20 <43004560+zhan-20@users.noreply.github.com>
2020-02-24 23:19:08 -0500

    Update sign_up.js

--- sign_up.js
+++ sign_up.js
@@ -1,17 +1,50 @@
 	function checkinput() {
-		if (document.getElementById("permit").value==="No"){
-		alert("You must sign the permission form before registering for an account!");}
-		var x =document.getElementById("email").value;
-		if(x==NULL) {
-			alert("You need to have an email address to open the registration link.");
+		var input1=document.getElementById("t-shirts").value;
+		var input2=document.getElementById("allergies").value;
+		if (input1.charAt(input1.length-1)!='!') {
+		alert("You need to end the t-shirt sizes input with an exclamation mark!");
+		return false;
 		}
-		var judge1=false;
-		var judge2=false;
-		for (j=0; j<x.length; j++) {
-			if (x.substring(j, j+1)=="@") judge1=true;
-			if (x.substring(j, j+1)==".") judge2=true;
+		if (input2.charAt(input2.length-1)!='!') {
+			alert("You need to end the allergies input with an exclamation mark!");
+			return false;
 		}
-		if (judge1==false || judge2==false) {
-			alert("You have entered an invalid email address. Please check again.");
+		var input3=document.getElementById("permit").value;
+		if (input3!=="Yes"&&input3!=="No") {
+			alert("You can only enter either 'Yes' or 'No'!");
+			return false;
 		}
+		var input4=document.getElementById("schoolname").value;
+		if (input4.length<8) {
+			alert("Please enter the full name of your school.");
+			return false;
+		}
+		if (checkinput1(input1)==false||checkinput1(input2)==false) {
+		alert("You need to include every team member once and only once; separate each two members by a semicolon and a space.");
+		return false;
+		}
+		return true;
+	}
+	function checkinput1(x) {
+		var p=0; var i; 
+		for (i=1; i<x.length-1; i++) {
+			if (x1(x, i)) p++;
+		}
+		var m=calc()-1;
+		if (p===m) return true;
+		return false;
+	}
+	function x1 (x, y) {
+		if (x.substring(y, y+2)==="; ") return true;
+		return false;
+	}
+	function calc() {
+		var numberofpeople=1;
+		var a2=document.getElementById("member2").value;
+		var a3=document.getElementById("member3").value;
+		var a4=document.getElementById("member4").value;
+		if (a2.length>0) numberofpeople++;
+		if (a3.length>0) numberofpeople++;
+		if (a4.length>0) numberofpeople++;
+		return numberofpeople;
 	}
